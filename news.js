e5d7651654c31b0fdd10b7437c96faea9f4d3fc9
Hassan Abass <abass.hassan.7798@gmail.com>
2022-12-10 13:39:25 +0100

    Update news.js

--- news.js
+++ news.js
@@ -1,11 +1,13 @@
 let menu = document.querySelector("#menu");
 let openMenu = document.querySelector("#open-menu");
 let closeMenu = document.querySelector("#close-menu");
+menu.style.display = "none";
 
 openMenu.addEventListener("click", open);
 function open() {
   menu.style.display = "block";
   menu.style.left = "30vw";
+  window.document.body.style.overflow = "hidden";
 }
 closeMenu.addEventListener("click", close);
 function close() {
